4ddd75c5050e7361a4282ab2f31cb83755be116b
Erik Malik <erikfarhanmalik@gmail.com>
2018-01-12 14:43:32 +0700

    - add default location for rest-api-dummy if not specifiled - add code specific response beside 200 - update README.md

--- src/main/java/com/erik/tools/controllers/MainController.java
+++ src/main/java/com/erik/tools/controllers/MainController.java
@@ -1,46 +1,88 @@
 package com.erik.tools.controllers;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-@RestController
+@Controller
 public class MainController {
 
 	@RequestMapping("/api/{json-filename}")
 	@CrossOrigin(origins = "*")
-	public ObjectNode index(@PathVariable("json-filename") String jsonFileName) {
+	public ResponseEntity<ObjectNode> index(@PathVariable("json-filename") String jsonFileName) {
 		String folder = System.getProperty("dummyapi.folder");
-		String path = folder + jsonFileName + ".json";
-		System.out.println("Json files location: " + path);
-		return getJsonResponse(path);
+		return getJsonResponse(folder, jsonFileName);
 	}
 
-	private ObjectNode getJsonResponse(String path) {
+	private ResponseEntity<ObjectNode> getJsonResponse(String folder, String jsonFileName) {
+		ResponseEntity<ObjectNode> response;
 		ObjectMapper mapper = new ObjectMapper();
 		ObjectNode node = mapper.createObjectNode();
 		try {
-			String jsonString = readFile(path);
+			String jsonString = "{}";
+			if (folder != null) {
+				String path = folder + jsonFileName + ".json";
+				System.out.println("Json files location: " + path);
+				jsonString = readFile(path);
+			} else {
+				String path = "jsons/" + jsonFileName + ".json";
+				System.out.println("Json files location: " + path);
+				InputStream stream = MainController.class.getClassLoader().getResourceAsStream(path);
+				StringBuilder textBuilder = new StringBuilder();
+				try (Reader reader = new BufferedReader(
+						new InputStreamReader(stream, Charset.forName(StandardCharsets.UTF_8.name())))) {
+					int c = 0;
+					while ((c = reader.read()) != -1) {
+						textBuilder.append((char) c);
+					}
+				}
+				jsonString = textBuilder.toString();
+			}
 			node = mapper.readValue(jsonString, ObjectNode.class);
 		} catch (FileNotFoundException e) {
-			node.put("message", path + " not found");
+			node.put("message", "Json not found");
 		} catch (IOException e) {
-			node.put("message", path + " throw IO Exception");
+			node.put("message", "Request throw IO Exception");
 		}
-		return node;
+		
+		int responseCode = getResponseCode(jsonFileName);
+		if(responseCode == 200) {			
+			response = ResponseEntity.ok(node);
+		} else {
+			response = ResponseEntity.status(responseCode).body(node);
+		}
+		return response;
 	}
-	
+
 	private String readFile(String path) throws IOException {
 		byte[] encoded = Files.readAllBytes(Paths.get(path));
 		return new String(encoded);
+	}
+
+	private int getResponseCode(String filename) {
+		int code = 200;
+
+		if (filename.matches("^(.*)?-\\d{3}$")) {
+			String[] split = filename.split("-");
+			code = Integer.valueOf(split[split.length-1]);
+		}
+
+		return code;
 	}
 }
